52c8d24004c7e942c04023b0502b890968628286
divinerapier <poriter.coco@gmail.com>
2019-03-13 23:31:49 +0800

    [ctci] 8.1.4

--- array-string/src/main.rs
+++ array-string/src/main.rs
@@ -1,5 +1,4 @@
 #![feature(str_as_mut_ptr)]
-
 use std::os::raw::c_char;
 
 fn main() {
@@ -11,11 +10,19 @@ fn main() {
     // println!("{}", char_in_string_is_unique_01("aba"));
     // println!("{}", char_in_string_is_unique_01("abcABC"));
     // println!("{}", char_in_string_is_unique_01("abcABCC"));
-    let mut s = "123".to_owned();
-    unsafe {
-        reverse(&mut s);
+    {
+        let mut s = "123".to_owned();
+        unsafe {
+            reverse(&mut s);
+        }
+        println!("{:?}", s);
     }
-    println!("{:?}", s);
+    {
+        let s = "Mr John Smith";
+        unsafe {
+            println!("{}", urlify(&s));
+        }
+    }
 }
 
 // 8.1.1
@@ -92,4 +99,85 @@ unsafe fn reverse(s: &mut String) {
         return;
     }
     c_reverse(s.as_mut_ptr() as *mut c_char);
+}
+
+// 8.1.4
+
+// URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
+// has sufficient space at the end to hold the additional characters, and that you are given the "true"
+// length of the string. (Note: If implementing in Java, please use a character array so that you can
+// perform this operation in place.)
+// EXAMPLE
+// Input: "Mr 3ohn Smit h 13
+// Output: "Mr%203ohn%20Smith"
+
+unsafe fn urlify(s: &str) -> String {
+    let result = c_urlify(s.as_ptr() as *const c_char);
+    let length = c_strlen(result);
+    String::from_raw_parts(result as *mut u8, length, length)
+}
+
+unsafe fn c_urlify(s: *const c_char) -> *mut c_char {
+    let count = c_str_count(s, ' ' as c_char);
+    let length = c_strlen(s);
+    let buffer: *mut c_char = malloc(length + count << 1);
+    println!("length: {}", length);
+    println!("count: {}", count << 1);
+    let (mut scursor, mut tcursor) = (s.offset(0), buffer.offset(0));
+
+    loop {
+        if scursor == (0 as *mut i8) || *scursor == ('\0' as i8) {
+            *tcursor = 0;
+            break;
+        }
+        if *scursor != (' ' as c_char) {
+            *tcursor = *scursor;
+        } else {
+            *tcursor = '%' as c_char;
+            *tcursor.offset(1) = '2' as c_char;
+            *tcursor.offset(2) = '0' as c_char;
+            tcursor = tcursor.offset(2);
+        }
+        scursor = scursor.offset(1);
+        tcursor = tcursor.offset(1);
+    }
+
+    return buffer;
+}
+
+unsafe fn malloc<T>(size: usize) -> *mut T
+where
+    T: Sized,
+{
+    let mut buffer: std::vec::Vec<T> = std::vec::Vec::with_capacity(size);
+    let result = buffer.as_mut_ptr();
+    std::mem::forget(buffer);
+    result
+}
+
+unsafe fn c_str_count(s: *const c_char, c: c_char) -> usize {
+    let mut count: usize = 0;
+    let mut ptr = s.offset(0);
+    loop {
+        if ptr == (0 as *const i8) || *ptr == ('\0' as i8) {
+            break;
+        }
+        ptr = ptr.offset(1);
+        if *ptr == c {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+unsafe fn c_strlen(s: *const c_char) -> usize {
+    let mut count: usize = 0;
+    let mut ptr = s.offset(0);
+    loop {
+        if ptr == (0 as *const i8) || *ptr == ('\0' as i8) {
+            break;
+        }
+        ptr = ptr.offset(1);
+    }
+    return count;
 }
